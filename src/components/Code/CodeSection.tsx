6e8b69a3f2b47351204713b4df406a2469e2d109
Valeriia <valeriaagapova.2001@gmail.com>
2024-07-20 15:42:42 +0300

    Added loader while checking code

--- src/components/Code/CodeSection.tsx
+++ src/components/Code/CodeSection.tsx
@@ -3,6 +3,7 @@ import './codeSection.scss'
 import { postCode } from '@api';
 import { setApplicationStep, applicationIdState } from '../../store/applicationSlice';
 import { useAppSelector, useAppDispatch } from '../../hooks';
+import { Loader } from '@UI';
 
 
 const CodeSection: FC = () => {
@@ -11,6 +12,7 @@ const CodeSection: FC = () => {
   const [error, setError] = useState(false);
   const [codes, setCodes] = useState<string[]>(['', '', '', '']);
   const codeRefs = [useRef<HTMLInputElement>(null), useRef<HTMLInputElement>(null), useRef<HTMLInputElement>(null), useRef<HTMLInputElement>(null)];
+  const [loading, setLoading] = useState(false);
 
   // фокус в первое поле при загрузке
   useEffect(() => {
@@ -50,36 +52,42 @@ const CodeSection: FC = () => {
 
   const handleSubmit = async (verificationCode: string) => {
     console.log(verificationCode);
+    setLoading(true);
     try {
       const result = await postCode(applicationId, verificationCode);
       if (result) {
           dispatch(setApplicationStep(7));
-          console.log("прошло")
+          setLoading(false);
       } else throw new Error();
     } catch (error) {
         console.error(error);
         setError(true);
+        setLoading(false);
     }
   };
 
   return (
     <section className="codeSection">
       <h2 className="codeSection__title">Please enter confirmation code</h2>
-      <div className="codeSection__code">
-        {codes.map((code, index) => (
-          <input
-            key={index}
-            type="number"
-            placeholder=""
-            className="codeSection__code-input"
-            maxLength={1}
-            value={code}
-            onChange={(e) => handleChange(index, e.target.value)}
-            onKeyDown={(e) => handleBackspace(index, e)}
-            ref={codeRefs[index]}
-          />
-        ))}
-      </div>
+      {
+        loading ? <Loader /> :
+          <div className="codeSection__code">
+            {codes.map((code, index) => (
+              <input
+                key={index}
+                type="number"
+                placeholder=""
+                className="codeSection__code-input"
+                maxLength={1}
+                value={code}
+                onChange={(e) => handleChange(index, e.target.value)}
+                onKeyDown={(e) => handleBackspace(index, e)}
+                ref={codeRefs[index]}
+              />
+            ))}
+          </div>
+      }
+      
       {error && <div className="codeSection__error">Invalid confirmation code</div>}
     </section>
   );

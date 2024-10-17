a08ce90d27a9f0c76182a63e319e6774965bd85e
Valeriia Agapova <vagapova@neoflex.ru>
2024-10-17 10:00:46 +0300

    add button storie

---
+++ src/components/UI/Button/Button.stories.tsx
@@ -0,0 +1,11 @@
+// import React from 'react';
+import { Button } from './Button';
+
+const BasicButton = () => <Button className='mainBtn'>Click me</Button>;
+
+export default {
+    title: 'Button',
+    component: Button,
+};
+
+export { BasicButton }
